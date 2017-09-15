33409fd8787997d978592d0f0addb739c1f63a4e
Megan Mercer <megan.mercer@inovexcorp.com>
2017-09-15 09:12:32 -0400

    Remove SplashScreen and utilize exisitng Expo properties for loading image. Upgrade Expo version and change exp.json to app.json per the spec. Add more to gitignore.

--- main.js
+++ main.js
@@ -3,22 +3,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import AppReducer from './reducers/AppReducer.js';
-import ConnectedRootNavigator from './navigators/RootNavigator.js';
+import AppNavigator from './navigators/AppNavigator.js';
 
 class App extends React.Component {
 
     store = createStore(
-        AppReducer,
         applyMiddleware(logger),
     );
 
     render() {
         return (
           <Provider store={this.store}>
-            <ConnectedRootNavigator />
+            <AppNavigator />
           </Provider>
         );
     }
 }
+
 Expo.registerRootComponent(App);
